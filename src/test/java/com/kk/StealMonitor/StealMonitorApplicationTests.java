cccb891753429c917515ebd8cca19f0bc8f66f21
Jerzy9 <zbylutkonrad@gmail.com>
2020-08-23 11:48:38 +0200

    Initial commit

---
+++ src/test/java/com/kk/StealMonitor/StealMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.StealMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StealMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
