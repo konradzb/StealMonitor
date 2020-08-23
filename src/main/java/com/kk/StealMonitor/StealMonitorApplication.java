820d2a7ae71ab1f8178dc7e346adfbf2c5d52651
Jerzy9 <zbylutkonrad@gmail.com>
2020-08-23 16:40:14 +0200

    Initial commit

---
+++ src/main/java/com/kk/StealMonitor/StealMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.kk.StealMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StealMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StealMonitorApplication.class, args);
+	}
+
+}
