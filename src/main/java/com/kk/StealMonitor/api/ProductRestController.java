87c046a9be93bcbe1f2604e006d59519c31e01d2
Jerzy9 <zbylutkonrad@gmail.com>
2020-08-25 21:07:58 +0200

    getAllTask-rest-works

--- src/main/java/com/kk/StealMonitor/api/ProductRestController.java
+++ src/main/java/com/kk/StealMonitor/api/ProductRestController.java
@@ -3,9 +3,17 @@ package com.kk.StealMonitor.api;
 import com.kk.StealMonitor.model.Product;
 import com.kk.StealMonitor.service.ProductGetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+@RestController
+@RequestMapping("api/v1/product")
 public class ProductRestController {
     private ProductGetService productGetService;
 
@@ -14,7 +22,19 @@ public class ProductRestController {
         this.productGetService = productGetService;
     }
 
+    @GetMapping
     public List<Product> getAllProducts() {
         return productGetService.getAllProducts();
+    }
+
+    @GetMapping("{sql}")
+    public List<Product> getProductsWithCustomSql(@PathVariable("sql") String sql) {
+        return productGetService.getProductsWithCustomSql(sql);
+    }
+
+    @GetMapping("{id}")
+    public Optional<Product> selectById(@PathVariable("id") UUID id) {
+        System.out.println("SELECTBYID");
+        return productGetService.selectProductById(id);
     }
 }
--- src/main/java/com/kk/StealMonitor/dao/product/FakeProductDaoAccessService.java
+++ src/main/java/com/kk/StealMonitor/dao/product/FakeProductDaoAccessService.java
@@ -29,26 +29,50 @@ public class FakeProductDaoAccessService implements ProductDao {
 
     @Override
     public List<Product> getAllProducts() {
+        //just for test
+        UUID id = UUID.randomUUID();
+        String name = "name";
+        String link = "link";
+        String siteName = "name";
+        String oldPrice = "old";
+        String newPrice = "new";
+        String remainingQuantity = "re";
+        String limitQuantity = "limit";
+        String img = "img";
+        String category = "category";
+
+        Product product = new Product(id,
+                link,
+                name,
+                siteName,
+                oldPrice,
+                newPrice,
+                remainingQuantity,
+                limitQuantity,
+                img,
+                category);
+        insertProduct(id,product);
+
         return products;
     }
 
     @Override
     public List<Product> getProductsWithCustomSql(String sql) {
+        //this won't be working in fakeProductDao
         return null;
     }
 
     @Override
-    public Optional<Product> selectTaskById(UUID id) {
+    public Optional<Product> selectProductById(UUID id) {
         return products.stream().filter(product -> product.getId().equals(id)).findFirst();
     }
 
     @Override
     public int deleteTask(UUID id) {
-        Optional<Product> product = selectTaskById(id);
+        Optional<Product> product = selectProductById(id);
         if(product.isEmpty())
             return 0;
-
-        products.remove(selectTaskById(id));
+        products.remove(product);
         return 1;
     }
 }
